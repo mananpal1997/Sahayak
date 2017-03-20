cc563a8ec0ee77487ea8f351f0df5ad0781e6b6a
Manan Pal Singh <brunodicosta@gmail.com>
2017-03-20 19:24:29 +0530

    admin access added for admin panel

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,8 +1,22 @@
 'use strict';
 
-module.exports = function(server) {
-  // Install a `/` route that returns server status
-  var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
-  server.use(router);
+module.exports = function(app) {
+  var User = app.models.User;
+  var Role = app.models.Role;
+  var RoleMapping = app.models.RoleMapping;
+
+  try {
+  	User.create({ username: 'admin', email: 'admin@sahayak.com', password: 'admin@admin'}, function(err, instance, created) {});
+  } catch (e) { }
+
+  Role.create({name: 'admin'}, function(err, role) {
+  	if(err)
+  		console.log("Not gonna tell. Find yourself :P");
+  	else {
+  		role.principals.create({
+  			principalType: RoleMapping.USER,
+  			principalId: 1
+  		}, function(err, principal) {});
+  	}
+  });
 };
