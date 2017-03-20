5e7f24c7ccd128b9bc3ff3e1e77b4ed2ac7c46ce
Manan Pal Singh <brunodicosta@gmail.com>
2017-03-21 00:54:34 +0530

    updated book_order

--- common/models/client.js
+++ common/models/client.js
@@ -10,6 +10,7 @@ module.exports = function(Client) {
 
 	global.client_name = "";
 	global.client_phone = "";
+	global.loc = "";
 
 	Client.book_worker = function(client_id, worker_id, task_info, service_id, location, report_time, duration, cb) {
 		async.series([
@@ -28,18 +29,46 @@ module.exports = function(Client) {
 				var Worker = app.models.Worker;
 				request.get("https://maps.googleapis.com/maps/api/geocode/json?latlng="+location+"&key=AIzaSyCXLOyTx9owpJEkv-RgboWAVCtllFmwjnQ", function(err, resp, body) {
 					Worker.findById(worker_id, function(err, instance) {
+						global.loc = (JSON.parse(body)).results[0].formatted_address;
 						p.send_message({
 							'src': '+919457226437',
 							'dst': '+91' + instance.phone_number,
 							'method': 'GET',
-							'text': 'Your service has been requested at ' + (JSON.parse(body)).results[0].formatted_address
-									+ '\nby' + global.client_name + ' on ' + String(report_time) + '. Contact number: '
-									+ global.client_phone
+							'text': 'Your service has been requested at ' + global.loc
+									+ '\nby ' + global.client_name + ' for ' + task_info + ' on ' + String(report_time)
+									+ '. Contact number: ' + global.client_phone
 						}, function(status, res) {
 							cb2();
-							cb();
 						});
 					});
+				});
+			},
+			function(cb3) {
+				var Service = app.models.Service;
+				Service.findById(service_id, function(err, instance) {
+					if(err)
+						console.log("Still not gonna tell the cause :P");
+					else {
+						var Task = app.models.Task;
+						Task.create({
+							'info': task_info,
+							'rate_decided': instance.rate,
+							'assigner': client_id,
+							'assignee': worker_id,
+							'location': global.loc,
+							'report_time': report_time,
+							'duration': duration,
+							'assigned': true,
+							'done': false
+						}, function(err, inst) {
+							if(err)
+								console.log("C'mon, write good code!");
+							else {
+								cb3();
+								cb();
+							}
+						});
+					}
 				});
 			}
 		]);
