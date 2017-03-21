8cc7b22abaf02510e0ffeb80aa1a98f05674fe85
sahil865gupta <sahil865gupta@gmail.com>
2017-03-21 09:58:11 +0530

    changes

--- website/controllers/servicesController.js
+++ website/controllers/servicesController.js
@@ -1,3 +1,9 @@
-app.controller("servicesController", function(){
-	
+app.controller("servicesController", function($scope, $http, api, toastr){
+	$http.get(api.apiUrl + "Services/")
+		.then(function(response){
+			console.log(response.data);
+			$scope.services = response.data;
+		}, function(error){
+			console.log(error.data);
+		});
 });
